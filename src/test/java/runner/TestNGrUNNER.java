500d013265278621e6bc9df45cace6f1c65fac39
karan kumar <karan.kumar.554@gmail.com>
2024-12-09 22:35:34 +0530

    karan

--- src/test/java/runner/TestNGrUNNER.java
+++ src/test/java/runner/TestNGrUNNER.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="C:\\Users\\karan\\selenium_framework_scratch\\deleteaftercompletion\\src\\test\\java\\sample\\launch.feature",
         glue="stepdefinition"
+
 )
 public class TestNGrUNNER  {
 	
