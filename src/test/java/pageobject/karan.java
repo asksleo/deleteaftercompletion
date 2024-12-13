3848975e349aafa35319575a630c088b487486ff
karan kumar <karan.kumar.554@gmail.com>
2024-12-13 07:14:26 +0530

    karan

--- src/test/java/pageobject/karan.java
+++ src/test/java/pageobject/karan.java
@@ -3,5 +3,6 @@ package pageobject;
 public class karan {
     public static void main(String args[]){
         System.out.println("karan kuamr one");
+        System.out.println("karan kuamr one");
     }
 }
