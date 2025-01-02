27baa7a74561f13f4723aa68edf0abfb7729b7a9
karan kumar <karan.kumar.554@gmail.com>
2025-01-02 14:27:46 +0530

    karan

--- src/test/java/stepDefinitions/Toplink2.java
+++ src/test/java/stepDefinitions/Toplink2.java
@@ -3,8 +3,13 @@ package stepDefinitions;
 import io.cucumber.java.en.Then;
 
 public class Toplink2 {
+    String s;
     @Then("user landed on  page")
     public void user_landed_on_toplinks_page() {
+        System.out.println("System.is.launched");
+        System.out.println(s.length());
+        System.out.println("System.is.launched");
+        System.out.println("System.is.launched");
         System.out.println("System.is.launched");
     }
 }
